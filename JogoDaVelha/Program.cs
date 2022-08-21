8a8122386cc31855d1c66928830bc04ca9bb72a9
artur <artur@DESKTOP-V0JU5PP>
2022-08-21 17:07:34 -0300

    save

--- JogoDaVelha/Program.cs
+++ JogoDaVelha/Program.cs
@@ -102,72 +102,72 @@ namespace JogoDaVelha
 
         static void Jogada()
         {
-            int contador = 0, linha, coluna;
-            bool verificador, verificaVitoria;
+            int contador = 0, linha, coluna, verificaVitoria = 0;
+            bool verificadorPosicaoOcupada;
             char x = 'X', o = 'O';
             char[,] jogoDaVelha = new char[3, 3];
-           
+
 
             do
             {
                 do
                 {
                     Console.WriteLine("Jogador X:");
-                    linha = Linha();
-                    coluna = Coluna();
+                    linha = Linha(); // chama funcao que pede linha
+                    coluna = Coluna(); // chama funcao que pede coluna
 
-                    if (VerificaPosicaoMatriz(linha, coluna, jogoDaVelha))
+                    if (VerificaPosicaoMatriz(linha, coluna, jogoDaVelha)) // verifica se a posicao esta disponivel para ser ocupada
                     {
-                        jogoDaVelha[linha, coluna] = x;
+                        jogoDaVelha[linha, coluna] = x; // como esta disponivel a posicao recebe X
                         Console.WriteLine("\tA ESCOLHA FOI: POSICAO [{0}, {1}] = {2}", linha, coluna, jogoDaVelha[linha, coluna]);
                         Console.WriteLine();
                         ImprimeTabuleiro(jogoDaVelha);
-                        
-                        contador++;
-                        verificador = false;
-                        verificaVitoria = Vitoria(jogoDaVelha, linha, coluna);
+
+                        contador++; // conta as jogadas do X (maximo de 5 pois é o primeiro a jogar)
+                        verificadorPosicaoOcupada = false; // recebe "false" pois nao estava ocupada
+                        verificaVitoria = Vitoria(jogoDaVelha, linha, coluna); // verifica se alguem ganhou (caso vitoria, verificaVitoria = 1)
                     }
                     else
                     {
-                        verificador = true;
+                        verificadorPosicaoOcupada = true;
                         Console.WriteLine("posicao ocupada");
                     }
-                } while (verificador);
+                } while (verificadorPosicaoOcupada); // enquanto for uma opc ocupada "true" manda digitar de novo ate q nao seja ocupado "false"
 
-                if (contador == 5)
+                if (contador == 9) // a funçao dar velha so pode ser chamada na ultima jogada, ou seja, count = 9 
                 {
                     DarVelha();
                 }
-                else if(contador < 5)
+                else if (contador < 9 && verificaVitoria == 0) // o jogo não atingiu o maximo de rodadas ainda (9), entao o X ja jogou e nao ganhou, entao falta o O jogar
                 {
                     do
                     {
                         Console.WriteLine("Jogador O:");
-                        linha = Linha();
-                        coluna = Coluna();
+                        linha = Linha(); // chama funcao que pede linha
+                        coluna = Coluna(); // chama funcao que pede coluna
 
-                        if (VerificaPosicaoMatriz(linha, coluna, jogoDaVelha))
+                        if (VerificaPosicaoMatriz(linha, coluna, jogoDaVelha)) // ver se a linha e a coluna que o O digitou esta disponivel
                         {
-                            jogoDaVelha[linha, coluna] = o;
+                            jogoDaVelha[linha, coluna] = o; // se esta disponivel, recebe O
 
                             Console.WriteLine("\tA ESCOLHA FOI: POSICAO [{0}, {1}] = {2}", linha, coluna, jogoDaVelha[linha, coluna]);
                             Console.WriteLine();
 
                             ImprimeTabuleiro(jogoDaVelha);
 
-                            verificador = false;
-
-                            verificaVitoria = Vitoria(jogoDaVelha, linha, coluna);
+                            contador++; // conta as jogadas do O (maximo de 4 pois é o segundo a jogar)
+                            verificadorPosicaoOcupada = false; // como a posicao estava livre, deve sair do laço e recebe falso para poder sair do laço
+                            verificaVitoria = Vitoria(jogoDaVelha, linha, coluna); // verifica se alguem ganhou (caso vitoria, verificaVitoria = 1)
                         }
                         else
                         {
-                            verificador = true;
+                            verificadorPosicaoOcupada = true; // se a posicao estiver ocupada, "true" nao pode sair do laço, executa novamente
                             Console.WriteLine("posicao ocupada");
                         }
-                    } while (verificador);
+                    } while (verificadorPosicaoOcupada); // enquanto a posicao esta ocupada == true, pede posicao novamente ate que nao esteja ocupada (verificaPosicaoOcupada == false)
                 }
 
-            } while (contador < 5);
+            } while (contador < 9 && verificaVitoria == 0); // enquanto nao atingiu o maximo de jogadas ainda (velha) ou ninguem ganhou o jogo ainda (vitoria). Passa a vez para o proximo jogador ate que de velha ou alguem ganhe
         }
 
         static bool VerificaPosicaoMatriz(int linha, int coluna, char[,] jogoDaVelha)
@@ -211,65 +211,72 @@ namespace JogoDaVelha
             Console.ReadKey();
         }
 
-        static void DarVelha()
+        static void DarVelha() // a velha so acontece depois que o X joga pois é ele quem começa e termina o jogo, 5 jogadas
         {
             Console.Clear();
             Console.WriteLine("o jogo empatou!");
             Console.ReadKey();
         }
 
-        static bool Vitoria(char[,] jogoDaVelha, int linha, int coluna)
+        static int Vitoria(char[,] jogoDaVelha, int linha, int coluna)
         {
-            bool vitoria = false;
+            int vitoria = 0;
             // verificacao X -------------------------------------------------------------------------
             // linhas
             if (jogoDaVelha[linha, 0] == 'X' && jogoDaVelha[linha, 1] == 'X' && jogoDaVelha[linha, 2] == 'X')
             {
                 GanhadorX();
-                vitoria = true;
+                vitoria = 1;
             }
+
             // colunas
             else if (jogoDaVelha[0, coluna] == 'X' && jogoDaVelha[1, coluna] == 'X' && jogoDaVelha[2, coluna] == 'X')
             {
                 GanhadorX();
-                vitoria = true;
+                vitoria = 1;
+
             }
             // diagonais
             else if (jogoDaVelha[0, 0] == 'X' && jogoDaVelha[1, 1] == 'X' && jogoDaVelha[2, 2] == 'X')
             {
                 GanhadorX();
-                vitoria = true;
+                vitoria = 1;
+
             }
             else if (jogoDaVelha[2, 0] == 'X' && jogoDaVelha[1, 1] == 'X' && jogoDaVelha[0, 2] == 'X')
             {
                 GanhadorX();
-                vitoria = true;
+                vitoria = 1;
+
             }
             // verificacao O -------------------------------------------------------------------------
             // linhas
             if (jogoDaVelha[linha, 0] == 'O' && jogoDaVelha[linha, 1] == 'O' && jogoDaVelha[linha, 2] == 'O')
             {
                 GanhadorO();
-                vitoria = true;
+                vitoria = 1;
+
             }
             // colunas
             else if (jogoDaVelha[0, coluna] == 'O' && jogoDaVelha[1, coluna] == 'O' && jogoDaVelha[2, coluna] == 'O')
             {
                 GanhadorO();
-                vitoria = true;
+                vitoria = 1;
+
             }
             // diagonais
             else if (jogoDaVelha[0, 0] == 'O' && jogoDaVelha[1, 1] == 'O' && jogoDaVelha[2, 2] == 'O')
             {
                 GanhadorO();
-                vitoria = true;
+                vitoria = 1;
+
             }
             else if (jogoDaVelha[2, 0] == 'O' && jogoDaVelha[1, 1] == 'O' && jogoDaVelha[0, 2] == 'O')
             {
                 GanhadorO();
-                vitoria = true;
-            }
+                vitoria = 1;
 
+            }
             return vitoria;
         }
         static void GanhadorX()
